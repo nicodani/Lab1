de35d4d9fb807a3b2acb3ede5647fe22f37314eb
nicodani <danescobar@udec.cl>
2016-03-26 18:36:01 +0000

    Codigo_Final_Lab1

--- Lab1.py
+++ Lab1.py
@@ -1,11 +1,17 @@
 #! /usr/bin/python
-from SimpleCV import Camera, Display, Image, Color
-img=Image("foto1.jpg")
-img=img.grayscale()
-img.show()
-img.save("gray.jpg")
-
-
+from SimpleCV import Camera, Display, Image
+import time
+c=Camera()
+img=c.getImage()
+img.save("Original.jpg")
+fot=img.show()
+time.sleep(4)
+fot.quit()
+img2=img.grayscale()
+fot2=img2.show()
+img2.save("EscalaGrises.jpg")
+time.sleep(4)
+fot2.quit()
 
 
 
